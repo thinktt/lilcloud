90ad57965e965be783a0a6f5fa04a1aedba580d3
thinktt <github@thinktt.e4ward.com>
2014-08-03 08:59:47 -0500

    adds mongo connection to the app
    These settings should allow a connection to the MongoLab instance in
    Heroku.

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var express = require('express'),
+	mongoose = require('mongoose'), 
     app = express(),
-    port = Number(process.env.PORT || 5000)
+    port = Number(process.env.PORT || 5000),
+    mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/devdb',
+    db
     ; 
+
+mongoose.connect(mongoUri);
+db = mongoose.connection;
+db.on('error', function () {
+  console.log('unable to connect to database at ' + mongoUri);
+});
+
 
 app.use('/', express.static('enigmaX/'));
 
