8d360b37294b9682317e56fe83801000b178f11d
thinktt <github@thinktt.e4ward.com>
2014-08-03 17:26:13 -0500

    use express-session and mongoose to store sessions

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express'),
 	mongoose = require('mongoose'), 
     app = express(),
-    port = Number(process.env.PORT || 5000),
+    port = Number(process.env.PORT || 3000),
     mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/devdb',
-    db
+    secret = process.env.SESSION_SECRET || 'non-secret secret for dev only',
+    db, mongoTest, sessionOptions
     ; 
 
+
+//...........MongoDB Connection.....................
 mongoose.connect(mongoUri);
 db = mongoose.connection;
 db.on('error', function () {
@@ -13,8 +16,20 @@ db.on('error', function () {
 });
 
 
+//.............Express Stack.....................
+
+app.use(require('express-session')({
+    key: 'session',
+    secret: secret,
+    store: require('mongoose-session')(mongoose),
+    saveUninitialized: true,
+  	resave: true
+}));
+
 app.use('/', express.static('enigmaX/'));
 
+
+//..........Start Server..........................
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
